a378b03f9272bd45c2a23be64edc41bdc16518a3
Steph Locke <steph@itsalocke.com>
2018-08-18 13:02:18 +0100

    I'll get there

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,10 @@
 
 const gulp = require('gulp')
- 
 require('hugo-search-index/gulp')(gulp)
  
+gulp.task('cpindex', function() {
+    gulp.src('search*')
+        .pipe(gulp.dest('./public/'));
+});
 
-
-gulp.task('default', ['hugo-search-index'])
+gulp.task('default', ['hugo-search-index', 'cpindex' ])
